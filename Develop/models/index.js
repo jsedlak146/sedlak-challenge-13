60a5c27d79083167f6d6f0d7358785cfcb16c5c1
jsedlak146 <jssedlak146@gmail.com>
2023-01-10 16:42:11 -0500

    index.js with model relations is finished

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id", // look at relations you made, these titles are what you need
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag, //this is example of pulling in 3rd table, only assocation
+  },
+  as: 'product_tag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'product_tag'
+});
 
 module.exports = {
   Product,
